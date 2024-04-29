4b7a0d98275d3e83671fa4dd7c30319f58c79fa6
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-04-29 23:53:31 +0530

    updating resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/TarunM_Resume.pdf";
+import pdf from "../../Assets/Tarun Manoharan.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
