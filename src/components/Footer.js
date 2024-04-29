75a59d2be51569a727eaf95c929df5cdda062c89
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-04-29 23:56:30 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/tarunmanoharan"
+                href="https://github.com/tarunmanoharann"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
