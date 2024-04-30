bc3a4bb45b72af723b8bb4da1170fba7e226fe59
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-04-30 10:03:24 +0530

    Update Footer.js
    update ig profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/_real_tarun_/"
+                href="https://www.instagram.com/real._.tarun/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
