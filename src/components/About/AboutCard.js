0c013396d1171a919a4978c7744f01a86fd0f2f6
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-06-11 21:31:21 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -29,7 +29,7 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(41,158,152)", fontWeight: "bold" }}>
-            "Lazy One Codes The best ..."{" "}
+            "One day (or) Day One !!"{" "}
           </p>
           <footer className="blockquote-footer">Tarun</footer>
         </blockquote>
