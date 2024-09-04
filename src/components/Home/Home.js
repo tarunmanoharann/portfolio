22816c291069b1a0b45b2acf10bf04e0dc5f2b43
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-09-04 07:38:03 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ function Home() {
              
 
               <h1 className="heading-name">
-                <strong className="main-name"> TARUN MANOHARAN</strong>
+                <strong className="main-name"> TARUN M</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
