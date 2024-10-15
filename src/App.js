27d15a7afd53de59de6a6fd2bfef6b26ad7ac9aa
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-10-15 10:50:37 +0530

    Update App.js
    preloader time

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       updateLoad(false);
-    }, 2500); 
+    }, 2000); 
 
     return () => clearTimeout(timer);
   }, []);
